0ce11e1df66641fb702ba35ac771edfd39cd74da
JHONATAN TIPAN <jhonatan.tipan@udla.edu.ec>
2024-01-24 16:04:57 -0500

    Implemetacion de la logica (abrir archivo, identificar el dato, pedir el dato)

--- Examen.c
+++ Examen.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 struct producto {
-    char codigo [25];
-    char nombreProd [50];
-    char descProd [50];
-    int cantidad ;
+    char codigo[25];
+    char nombreProd[50];
+    char marca[50];
+    int cantidad;
     float precioCompra;
 };
 
@@ -21,17 +21,43 @@ int main() {
         scanf("%d", &opcion);
 
         switch (opcion) {
+
+            //Crear archivo y escribir en el archivo .txt
             case 1:
+                archivo = fopen("productos.txt", "a");
                 
+                printf("Ingrese el codigo del producto: ");
+                scanf("%s", p.codigo);
+                fprintf(archivo, "%s\t    \t\t", p.codigo);
+                    
+                printf("Ingrese el nombre del producto: ");
+                scanf("%s", p.nombreProd);
+                fprintf(archivo, "\t%s\t    \t\t", p.nombreProd);
+                    
+                printf("Ingrese la marca: ");
+                scanf("%s", p.marca);
+                fprintf(archivo, "\t%s\t    \t\t", p.marca);
+                    
+                printf("Ingrese la cantidad: ");
+                scanf("%d", &p.cantidad);
+                fprintf(archivo, "\t%d\t     \t\t", p.cantidad);
+                    
+                printf("Ingrese el precio de compra: ");
+                scanf("%f", &p.precioCompra);
+                fprintf(archivo, "\t%.2f\n", p.precioCompra);
+                    
+                fclose(archivo);
+                printf("Datos guardados en el archivo productos.txt\n");
+               
                 break;
 
             case 2:
-                printf("Mostrando datos...\n");
-              
+                printf("Mostrando datos\n");
+                
                 break;
 
             case 3:
-                printf("Saliendo del programa...\n");
+                printf("Saliendo del programa\n");
                 break;
 
             default:
